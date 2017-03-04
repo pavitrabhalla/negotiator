c90f46dfb2943bb49581356b78c75897d83f0703
Pavitra Bhalla <pavitrabhalla@gmail.com>
2017-03-03 16:29:10 -0800

    Form and plotly integration working

--- main.go
+++ main.go
@@ -1,39 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
+
+	"fmt"
+
+	"github.com/plotly/golang-api/plotly"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/graph", postHandler)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println(err)
-	}
+	log.Println("Listening...")
+	http.ListenAndServe(":8000", nil)
 }
 
-type indexOject struct {
-	Name string
+type grant struct {
+	shares            int
+	strikePrice       int
+	commencement      time.Time
+	vestingCliff      int
+	vestingPeriod     int
+	vestingSchedule   string
+	outstandingShares int
+	companyValuation  int
+	numRounds         int
+	roundDetails      []round
 }
 
-func indexHandler(res http.ResponseWriter, req *http.Request) {
-	obj := indexOject{
-		Name: req.FormValue("name"),
+type round struct {
+	valuation    int
+	amountRaised int
+}
+
+func postHandler(res http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	log.Println(req.Form)
+	f := plotly.Figure{
+		Data: []plotly.Trace{
+			plotly.Trace{
+				Type: "scatter",
+				X: plotly.Array{
+					4.54, 3, 34, 35, 362,
+				},
+				Y: plotly.Array{
+					1, 2, 3, 4, 5,
+				},
+			},
+		},
 	}
 
-	// Get template.
-	f, err := ioutil.ReadFile("web/index.html")
+	result, err := f.Save("new golang file")
 	if err != nil {
-		res.WriteHeader(400)
+		fmt.Printf("Error happened %v", err)
 		return
 	}
 
-	tmpl := template.Must(template.New("index").Parse(string(f)))
-	if err := tmpl.Execute(res, obj); err != nil {
-		res.WriteHeader(400)
-		return
-	}
+	http.Redirect(res, req, result.Url, 301)
 }
